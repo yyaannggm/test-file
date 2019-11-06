aff7827cf742fc01d42006fb13b476f2e80701e1
mengyang <mengyang@192.168.1.100>
2019-11-07 00:27:46 +0800

    hello world

---
+++ hello world/hello world/main.c
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    cout << "hello world" << endl;
+    for(int i = 0;i < 100;i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
+    for(int i = 0;i < 100;i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
