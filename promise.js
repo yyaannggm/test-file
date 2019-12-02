f8dd6fbd3d7c3e459199347b283e593e2a5b7397
yyaannggm <15001760091@163.com>
2019-12-03 00:58:30 +0800

    promise

--- promise.js
+++ promise.js
@@ -4,3 +4,13 @@ function sleep(ms) {
   })
 }
 sleep(500).then(() => console.log("finished"));
+
+
+
+let promise = new Promise(function (resolve, reject) {
+  console.log("1");
+  resolve();
+});
+setTimeout(() => console.log("2"), 0);
+promise.then(() => console.log("3"));
+console.log("4");
